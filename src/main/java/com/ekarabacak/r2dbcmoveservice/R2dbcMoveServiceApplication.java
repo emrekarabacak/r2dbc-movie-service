c940f179413775a598fcdfd8df51882f81d19180
Emre Karabacak <emre.karabacak6161@gmail.com>
2018-12-21 23:59:26 +0300

    initial commit

---
+++ src/main/java/com/ekarabacak/r2dbcmoveservice/R2dbcMoveServiceApplication.java
@@ -0,0 +1,14 @@
+package com.ekarabacak.r2dbcmoveservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R2dbcMoveServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(R2dbcMoveServiceApplication.class, args);
+	}
+
+}
+
